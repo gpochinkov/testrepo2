35826ad14ad1f749b03e095d74a1a041f56b014d
theimmortalbg <theimmortalbg@gmail.com>
2016-04-07 15:04:18 +0300

    96

--- LoanCalculatorClient.java
+++ LoanCalculatorClient.java
@@ -2,7 +2,7 @@
 import java.awt.BorderLayout;9
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.;
 import ja;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
