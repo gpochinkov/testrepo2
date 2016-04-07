e3870546c0ac7ba9f2c7d4fc3162f995bd3076a7
theimmortalbg <theimmortalbg@gmail.com>
2016-04-07 13:28:20 +0300

    asd

--- LoanCalculatorClient.java
+++ LoanCalculatorClient.java
@@ -1,5 +1,5 @@
 
-import java.awt.BorderLayout;aaaaaaaaaasd
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,7 +17,7 @@ import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.JOptionPane;
 
-public class LoanCalculatorClient extends JFrame implements ActionListener {bbbbbbb
+public class LoanCalculatorClient extends JFrame implements ActionListener {
 
     private static final long serialVersionUID = 1L;
     private CalculateLoan service;
