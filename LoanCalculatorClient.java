0f943612ee9b12ded306490f5c259f9d4357fc12
georgi <gpochinkov@motivian.bg>
2016-04-07 11:52:43 +0300

    dffdfdf

--- LoanCalculatorClient.java
+++ LoanCalculatorClient.java
@@ -19,7 +19,7 @@ import javax.swing.JOptionPane;
 
 public class LoanCalculatorClient extends JFrame implements ActionListener {ffdgdfgdf
 
-    private static final long serialVersionUID = 1L;local1
+    private static final long serialVersionUID = 1L;local2
     private CalculateLoan service;remote2
     private JTextField interest;
     private JTextField years;
